6ef6e738a57163a0eacd58f5b7429e711b7a77f3
PetarElez <petarelez21@gmail.com>
2019-08-12 11:15:17 +0200

    inital commit

---
+++ src/com/chess/engine/pieces/Pawn.java
@@ -0,0 +1,57 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static com.chess.engine.board.Move.*;
+
+public class Pawn extends Piece {
+
+    private final static int[] CANDIDATE_MOVES_COORDINATES={8,16,7,9};
+
+    Pawn(final int piecePosition,final Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves=new ArrayList<>();
+
+        for(final int currentCandidateOffset:CANDIDATE_MOVES_COORDINATES){
+           final int candidateDestinatioinCoordiante=this.piecePosition+(this.pieceAlliance.getDiresction()*currentCandidateOffset);
+
+            if(!BoardUtils.isValidTileCoordinate(candidateDestinatioinCoordiante)){
+                continue;
+            }
+
+            if(currentCandidateOffset==8 && !board.getTile(candidateDestinatioinCoordiante).isTileOccupied()){
+                legalMoves.add(new MajorMove(board,this,candidateDestinatioinCoordiante));
+            }else if(currentCandidateOffset==16 && this.isFirstMove() &&
+                    (BoardUtils.SECOND_ROW[this.piecePosition] && this.getPieceAllience().isBlack()) ||
+                    (BoardUtils.SEVENTH_ROW[this.piecePosition] && this.getPieceAllience().isWhite())){
+                final int behindCandidateDestinationCoordinate=this.piecePosition+(this.getPieceAllience().getDiresction()*8);
+                if(!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                    !board.getTile(candidateDestinatioinCoordiante).isTileOccupied()){
+
+                    legalMoves.add(new MajorMove(board,this,candidateDestinatioinCoordiante));
+
+                }
+            }else if(currentCandidateOffset==7 &&
+                    ((BoardUtils.EIGHT_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() ) ||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())) ){
+
+            }else if(currentCandidateOffset==9){
+
+            }
+        }
+
+
+        return legalMoves;
+    }
+}
